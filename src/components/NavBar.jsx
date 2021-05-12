923cd8e111b39426e591fb2c02e17edd298ce9f9
vireal01 <vireal014@yandex.ru>
2021-05-12 21:22:33 +0300

    first commit

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const NavBar = () => {
+   return (
+      <nav className='nav'>
+         <div>
+            <a>Profile</a>
+         </div>
+         <div>
+            <a>Message</a>
+         </div>
+         <div>
+            <a>News</a>
+         </div>
+         <div>
+            <a>Settings</a>
+         </div>
+      </nav>
+   );
+}
+
+export default NavBar;
---
+++ src/components/profile.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+const Profile = () => {
+   return (
+      <div className="content">
+         <div>
+            <img src='https://static01.nyt.com/images/2016/08/05/us/05onfire1_xp/05onfire1_xp-jumbo-v2.jpg' alt='' />
+         </div>
+         <div>
+            lala
+         </div>
+      </div>
+   )
+}
+
+export default Profile;
